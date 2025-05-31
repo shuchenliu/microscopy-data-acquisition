d06e36480d6ad7bb06a1396f6ee3e8890f6ce50c
shuchenliu <shuchenliu2014@gmail.com>
2025-05-31 00:33:13 -0700

    update epfl script connection number

--- src/query/epfl.sh
+++ src/query/epfl.sh
@@ -4,5 +4,5 @@
 DATA_FILE=https://documents.epfl.ch/groups/c/cv/cvlab-unit/www/data/%20ElectronMicroscopy_Hippocampus/volumedata.tif
 
 # Data nature: single large file
-# utilize aria2c multi-connection support for files of larger sizes
-aria2c -x 8 $DATA_FILE
+# utilize aria2c multi-connection / multi-part support for files of larger sizes
+aria2c -x 20 $DATA_FILE
