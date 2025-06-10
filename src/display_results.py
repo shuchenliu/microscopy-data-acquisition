844527c42add4432a3c131c1e24356c42c5f3b3e
shuchenliu <shuchenliu2014@gmail.com>
2025-06-09 17:55:28 -0700

    add time used to display

--- src/display_results.py
+++ src/display_results.py
@@ -1,20 +1,21 @@
 import contextlib
 from multiprocessing import Manager
 from subprocess import CompletedProcess
-from time import sleep
+from time import sleep, time
 
 from rich.live import Live
 from rich.table import Table
 
 SPINNER_FRAMES = ["|", "/", "-", "\\"]
 
-def render_table(table_name, col_names, labels, results, outputs, frame):
+def render_table(table_name, col_names, labels, frame, results, outputs, time_used, ):
     table = Table(title=table_name)
     for col_name in col_names:
         table.add_column(col_name)
 
     if any(v is not None for v in outputs.values()):
-        table.add_column('Output')
+        table.add_column('Output', justify='center')
+        table.add_column('Time used', justify='right')
 
     for label in labels:
         result = results[label]
@@ -22,6 +23,7 @@ def render_table(table_name, col_names, labels, results, outputs, frame):
         row = list([label, result + spinner])
         if outputs[label] is not None:
             row.append(outputs[label])
+            row.append(time_used[label])
         table.add_row(*row)
 
     return table
@@ -32,20 +34,24 @@ def display_table(*, table_name, col_names, labels):
     # wrap frame so we can pass by reference
     frame = [0]
 
-    def render_proxy(_status, _outputs):
-        return render_table(table_name, col_names, labels, _status, _outputs, frame[0])
+    def render_proxy(*_passed):
+        return render_table(table_name, col_names, labels, frame[0], *_passed)
 
     with Manager() as manager:
         completed = manager.Value("i", 0)
         status = manager.dict({label: "Running..." for label in labels})
         outputs = manager.dict({label: None for label in labels})
+        time_used = manager.dict()
 
-        passed = [status, outputs]
+        start_time = time()
+
+        passed = [status, outputs, time_used]
 
         def update_status(result: CompletedProcess):
             label, res, output = result
             status[label] = f"[green]Done[/green]" if res.returncode == 0 else f"❌ Failed ({res.returncode})"
             outputs[label] = output
+            time_used[label] = f"{time() - start_time:.2f}s"
             completed.value += 1
 
         with Live(render_proxy(*passed), refresh_per_second=10) as live:
