90691202f2c0385c4d3e2270406e1fa396f03454
shuchenliu <shuchenliu2014@gmail.com>
2025-06-10 02:39:24 -0700

    include metadata to EMPIAR dataset

--- src/query/empiar.sh
+++ src/query/empiar.sh
@@ -27,7 +27,7 @@ TOKEN=$(curl -s $EMPIAR_URI -c $COOKIE_JAR \
 # file IDs we'd need for the server to generate .zip
 ## smaller test ids
 #PARENTS=$(echo {18464846..18464846} | tr ' ' '-')
-PARENTS=$(echo {18464846..18465029} | tr ' ' '-')
+PARENTS=$(echo {18464846..18465029} | tr ' ' '-')-19026452
 
 
 # define output name
