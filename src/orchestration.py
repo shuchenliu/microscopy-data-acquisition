69297ab4726a146ecd5779ec621f2be8cbba7bd6
shuchenliu <shuchenliu2014@gmail.com>
2025-06-10 23:23:51 -0700

    implement grouped sequential execution of tasks

--- src/orchestration.py
+++ src/orchestration.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
-from multiprocessing import Pool, cpu_count, dummy
+import time
+from multiprocessing import Pool, cpu_count, dummy, Manager
 from subprocess import CompletedProcess
 
 from utils.keygen import keygen
@@ -9,11 +10,11 @@ from utils.display_results import display_table
 
 TASKS = [
         # 'test',
+        'hemibrain',
+        'ome-idr',
+        'janelia-openorganelle',
         'empiar',
         'epfl',
-        'janelia-openorganelle',
-        'ome-idr',
-        'hemibrain',
     ]
 
 def get_output_dir(script_name: str) -> str:
@@ -75,12 +76,41 @@ def main():
     # generate gcp cred
     keygen(255)
 
-    with dummy.Pool(cpu_count()) as pool, \
-        display_table(table_name="Microscopy Data Query Results", col_names=["Task", "Status"], labels=TASKS) as update_status:
-            for sc in TASKS:
-                pool.apply_async(execute_script, args=(sc,), callback=update_status)
+    with Manager() as manager, \
+        dummy.Pool(cpu_count()) as pool, \
+        display_table(table_name="Microscopy Data Query Results", col_names=["Task", "Status"], labels=TASKS) as (mark_start, update_status):
+
+            # we separate tasks into 3 groups and run them sequentially
+            group0 = TASKS[0:2]
+            group1 = TASKS[2:4]
+
+            # we isolate EPFL since it's VERY slow and does need the full bandwidth
+            group2 = TASKS[4:]
 
 
+            groups = [group0, group1, group2]
+            lock = manager.Lock()
+
+            # when a group finishes, kick off the next group if possible
+            def launch_group(index: int):
+                remaining = manager.Value(int, len(groups[index]))
+
+                def on_task_complete(res):
+                    with lock:
+                        remaining.value -= 1
+
+                    if remaining.value == 0 and index < len(groups) - 1:
+                        launch_group(index + 1)
+
+                    update_status(res)
+
+                for i, sc in enumerate(groups[index]):
+                    mark_start(sc)
+                    pool.apply_async(execute_script, args=(sc,), callback=on_task_complete)
+
+
+            # start the first group
+            launch_group(0)
 
 if __name__ == "__main__":
     main()
